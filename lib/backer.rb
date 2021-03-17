b97a20e647474e2072cad0cff3108102bc160edc
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-17 23:38:09 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,10 +8,12 @@ class Backer
 
   def back_project(project_instance)
     @backed_projects << project_instance
+    project_instance.backers << self
   end
 
 
 
+# How do I access the @backers Project instance variable from the Backer class?
 
 
 end
